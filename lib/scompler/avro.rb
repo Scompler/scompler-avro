05148f93c5f09f3ff8c807eadfe0375bc823ce34
Alex Pylko <alexpylko@gmail.com>
2021-06-11 17:04:53 +0200

    Initial gem definition

---
+++ lib/scompler/avro.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'avro/schema_store'
+require_relative 'avro/deserializer'
+require_relative 'avro/serializer'
+
+module Scompler
+  module Avro
+    REGISTRY_NAME = 'scompler'
+  end
+end
---
+++ lib/scompler/avro/serializer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Scompler
+  module Avro
+    class Serializer
+      attr_reader :avro, :schema_name, :version_number
+
+      def initialize(avro:, schema_name: nil, version_number: nil)
+        @avro = avro
+        @schema_name = schema_name
+        @version_number = version_number
+      end
+
+      def call(content, schema_name: nil, version_number: nil)
+        schema_name ||= self.schema_name
+        version_number ||= self.version_number
+
+        avro.encode(content, schema_name: schema_name, namespace: version_number)
+      end
+    end
+  end
+end
