a9101e88ec29ace59101c4e5a5d3a72bdcf0b38f
Alex Pylko <alexpylko@gmail.com>
2021-11-15 23:27:58 +0100

    Fixed rubocop warnings

--- lib/scompler/avro/configuration.rb
+++ lib/scompler/avro/configuration.rb
@@ -8,13 +8,13 @@ module Scompler
       extend Dry::Configurable
 
       setting :cache, SchemaCache.new
-      setting :logger, ActiveSupport::Logger.new(STDOUT)
+      setting :logger, ActiveSupport::Logger.new($stdout)
 
       setting :cache_options do
         setting :force, false
         setting :skip_nil, true
         setting :compress, true
-        setting :expires_in, 86400
+        setting :expires_in, 86_400
         setting :version
         setting :race_condition_ttl
       end
